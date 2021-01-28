4f8b409b007c309c140412b206b758ea3b62a7f5
Markaa10 <rahulrajdahal@gmail.com>
2021-01-28 19:08:22 +0545

    smallButton

--- components/SmallButton.js
+++ components/SmallButton.js
@@ -1,27 +1,27 @@
-// import React from "react";
-// import { Image, View, Text } from "react-native";
-// import { FONTS, SIZES, COLORS } from "../constants";
+import React from "react";
+import { Image, View, Text } from "react-native";
+import { FONTS, SIZES, COLORS } from "../constants";
 
-// const SmallButton = ({ icon, text, textColor, bgcolor }) => {
-//   return (
-//     <View
-//       style={{
-//         display: "flex",
-//         flexDirection: "row",
-//         justifyContent: "space-between",
-//         alignItems: "center",
-//         borderRadius: SIZES.borderRadius,
-//         backgroundColor: bgcolor,
-//         width: 154,
-//         height: 60,
-//         paddingVertical: 21,
-//         paddingHorizontal: 34,
-//       }}
-//     >
-//       <Image source={icon} style={{ marginRight: 12 }} />
-//       <Text style={{ color: textColor, ...FONTS.h2 }}>{text}</Text>
-//     </View>
-//   );
-// };
+const SmallButton = ({ icon, text, textColor, bgcolor }) => {
+  return (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderRadius: SIZES.borderRadius,
+        backgroundColor: bgcolor,
+        width: 154,
+        height: 60,
+        paddingVertical: 21,
+        paddingHorizontal: 34,
+      }}
+    >
+      <Image source={icon} style={{ marginRight: 12 }} />
+      <Text style={{ color: textColor, ...FONTS.h2 }}>{text}</Text>
+    </View>
+  );
+};
 
-// export default SmallButton;
+export default SmallButton;
