2e2f384d7db14888f7abbf175980cbe85d9778a4
Rahul Raj Dahal <rahulrajdahal@gmail.com>
2026-01-28 14:25:06 +0000

    fix: remove navbar from notitication

--- screens/Notifications.js
+++ screens/Notifications.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { View, Text, Image } from "react-native";
-import { Navbar, NotificationCard } from "../components";
-import { COLORS, FONTS, icons, images, SIZES } from "../constants";
+import { Text, View } from "react-native";
+import { NotificationCard } from "../components";
+import { COLORS, FONTS } from "../constants";
 import { NoNotification } from "../constants/images";
 
 const Notifications = ({ navigation }) => {
@@ -46,29 +45,25 @@ const Notifications = ({ navigation }) => {
       {
         _id: 1,
         title: "New Challenge Awaits",
-        desc:
-          "For this month in Web Dev Challenge, we have partnered with our friends at Github, Gitlab, Icons8 and AWS. Learn More",
+        desc: "For this month in Web Dev Challenge, we have partnered with our friends at Github, Gitlab, Icons8 and AWS. Learn More",
         time: "5m",
       },
       {
         _id: 2,
         title: "New Event",
-        desc:
-          "Join us this week in our virtual webinar for 'Careers in UX Design'",
+        desc: "Join us this week in our virtual webinar for 'Careers in UX Design'",
         time: "9h",
       },
       {
         _id: 3,
         title: "Congratulations",
-        desc:
-          "Your article has been viewed 32.2k times and loved by 58 developers.",
+        desc: "Your article has been viewed 32.2k times and loved by 58 developers.",
         time: "11h",
       },
       {
         _id: 4,
         title: "Item Shipped",
-        desc:
-          "Your Item “Coder Mug” - has been shipped. You will receive it in 3 days (estimate)",
+        desc: "Your Item “Coder Mug” - has been shipped. You will receive it in 3 days (estimate)",
         time: "2d",
       },
     ];
@@ -144,8 +139,6 @@ const Notifications = ({ navigation }) => {
         height: "100%",
       }}
     >
-      <Navbar isActive />
-
       {/* Page Title And Settings */}
       {renderPageTitleRow()}
 
